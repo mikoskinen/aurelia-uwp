212e87399acc4f43091fb2fbd8b2cb83be5a8b0a
Mikael Koskinen <mijapkos@gmail.com>
2017-03-06 18:15:15 +0200

    Added back button

---
+++ AureliaUWP/backbutton.js
@@ -0,0 +1,26 @@
+﻿(function () {
+
+    var systemNavigationManager = Windows.UI.Core.SystemNavigationManager.getForCurrentView();
+    systemNavigationManager.addEventListener("backrequested", handleSystemNavigationEvent.bind(this));
+
+    window.onpopstate = function () {
+
+        var systemNavigation = Windows.UI.Core.SystemNavigationManager.getForCurrentView();
+
+        if (endsWith(window.location.href, "index.html")) {
+            systemNavigation.appViewBackButtonVisibility = Windows.UI.Core.AppViewBackButtonVisibility.collapsed;
+        } else {
+            systemNavigation.appViewBackButtonVisibility = Windows.UI.Core.AppViewBackButtonVisibility.visible;
+        }
+    };
+
+})();
+
+function handleSystemNavigationEvent(args) {
+    args.handled = true;
+    window.history.back();
+}
+
+function endsWith(str, suffix) {
+    return str.slice(-suffix.length) === suffix;
+}
